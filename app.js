18813eefb6536cd7abaef1c936a8ab040e7c0c3d
Anshu <anshugupta20560@gmail.com>
2024-06-12 17:13:52 +0530

    Unnecessary code removed

--- app.js
+++ app.js
@@ -23,10 +23,8 @@ app.get("/", (req, res) => {
 app.get("/assignments", (req, res) => {
   const directoryPath = path.join(__dirname, "assignments_folder");
   const { query, pathname } = url.parse(req.url, true);
-  // console.log(query.id);
   if (query.id === undefined) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    // res.write("Hello World");
     let template1 =
       "<html><body><form action='/assignments' enctype='multipart/form-data' method='POST'> <input type='file' name = 'uploaded_file'><input type = 'submit' value='Upload a file'></body></html>";
     res.write(template1);
@@ -40,15 +38,11 @@ app.get("/assignments", (req, res) => {
         (file) => path.extname(file).toLowerCase() === ".pdf"
       );
       console.log(pdfFiles);
-      // res.writeHead(200, { "Content-Type": "text/html" });
       res.write("<center><h2>Assignments</h2></center>");
 
       pdfFiles.forEach((file) => {
         encoded_file_name = encodeURIComponent(file);
         let file_path = "?id=" + encoded_file_name;
-        // res.writeHead(200, { "Content-Type": "text/html" });
-        // console.log(`<h3><a href =${file_path}>${file}</a></h3>`);
-        // res.write("<html><body><h1>Hello World</h1></body></html>");
         res.write(
           `<center><h3><a href =${file_path} target="_blank">${file}</a></h3></center>`
         );
@@ -62,13 +56,10 @@ app.get("/assignments", (req, res) => {
       res.writeHead(200, { "Content-Type": "application/pdf" });
       res.end(data);
     });
-    // res.send(directoryPath);
   }
-  // res.end();
 });
 
 app.post("/assignments", upload.single("uploaded_file"), (req, res) => {
-  // res.json(req.file);
   res.send("Uploaded successfully");
 });
 const port = process.env.PORT || 3000;
